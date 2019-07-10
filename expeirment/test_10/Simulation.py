e5c68f05af0008602ab8dee3d4211a88c5476e52
kotaro912214 <kotaro.seki1216@gmail.com>
2019-07-10 21:15:41 +0900

    rewrite the path dealing

--- expeirment/test_10/Simulation.py
+++ expeirment/test_10/Simulation.py
@@ -1,13 +1,29 @@
+import get_station
+from pathlib import Path, PureWindowsPath
+
 class Simulation():
 
-  def __init__(self, *args, **kwargs):
-    self.NUMBER_OF_EMPLOYEES = 4
-    self.TIME = 60 * 8
-    self.C_IN = 100
-    self.C_OUT = 205
-    self.C_E = 10000 * (self.TIME / (8*60))
-    self.PRICE_PER_15 = 205
-    self.FUEL_CONSUMPTION = 35
-    self.NUMBER_OF_STATIONS = 10
+  def __init__(self, params={
+    'NUMBER_OF_EMPLOYEES': 4,
+    'TIME': 60 * 8,
+    'C_IN': 100,
+    'C_OUT': 205,
+    'C_E_FULL': 10000,
+    'PRICE_PER_15': 205,
+    'FUEL_CONSUMPTION': 35,
+    'NUMBER_OF_STATIONS': 10
+  }):
+    self.NUMBER_OF_EMPLOYEES = params['NUMBER_OF_EMPLOYEES']
+    self.TIME = params['TIME']
+    self.C_IN = params['C_IN']
+    self.C_OUT  = params['C_OUT']
+    self.C_E_FULL  = params['C_E_FULL']
+    self.PRICE_PER_15  = params['PRICE_PER_15']
+    self.FUEL_CONSUMPTION  = params['FUEL_CONSUMPTION']
+    self.NUMBER_OF_STATIONS  = params['NUMBER_OF_STATIONS']
+    self.C_E_DAY = self.C_E_FULL * (self.TIME / 8 * 60)
 
-  
+
+
+if (__name__ == '__main__'):
+  print('hs')
--- expeirment/test_10/get_station.py
+++ expeirment/test_10/get_station.py
@@ -6,10 +6,14 @@ import time
 import urllib.request, json
 import urllib.parse
 import csv
+from pathlib import Path, PureWindowsPath
+
+base_path = PureWindowsPath(Path.cwd())
 
 # read SID from another file for security perspective
 def readSID():
-  sid_path = '../../get_station/sid.txt'
+  sid_path = base_path / 'sid.txt'
+  print(sid_path)
   f = open(sid_path)
   SID = f.read()
   return SID
@@ -43,61 +47,61 @@ def writeConst(const_name, const_value):
   f = open('./const.csv', 'a', encoding='utf-8')
   f.write(const_name + ',' + str(const_value) + '\n')
 
+if (__name__ == '__main__'):
+  KIND_OF_AIP = {'spot_list':'/spot/list?', 'category_list': '/category/list?', 'route': '/route?'}
 
-KIND_OF_AIP = {'spot_list':'/spot/list?', 'category_list': '/category/list?', 'route': '/route?'}
+  NUMBER_OF_STATIONS = 10
 
-NUMBER_OF_STATIONS = 10
+  # set the params for spot list request
+  params_spot = {'category': '', 'coord': '', 'radius': '', 'limit': '', 'datum': ''}
+  params_spot['category'] = '0817001002' # category code of careco carsharing
+  params_spot['coord'] = '35.689296,139.702089' # a coord of shinjuku station
+  params_spot['radius'] = '100000'
+  params_spot['limit'] = str(NUMBER_OF_STATIONS)
+  params_spot['datum'] = 'tokyo'
 
-# set the params for spot list request
-params_spot = {'category': '', 'coord': '', 'radius': '', 'limit': '', 'datum': ''}
-params_spot['category'] = '0817001002' # category code of careco carsharing
-params_spot['coord'] = '35.689296,139.702089' # a coord of shinjuku station
-params_spot['radius'] = '100000'
-params_spot['limit'] = str(NUMBER_OF_STATIONS)
-params_spot['datum'] = 'tokyo'
+  # get the data of the station list
+  request = makeRequest(KIND_OF_AIP['spot_list'], params_spot)
+  json_res = getResponse(request)
 
-# get the data of the station list
-request = makeRequest(KIND_OF_AIP['spot_list'], params_spot)
-json_res = getResponse(request)
-
-spots = json_res['items']
-S_info = []
-S_coord = []
-for spot in spots:
-  S_coord.append((spot['coord']['lat'], spot['coord']['lon']))
-  S_info.append([spot['name'], spot['coord']['lat'], spot['coord']['lon']])
+  spots = json_res['items']
+  S_info = []
+  S_coord = []
+  for spot in spots:
+    S_coord.append((spot['coord']['lat'], spot['coord']['lon']))
+    S_info.append([spot['name'], spot['coord']['lat'], spot['coord']['lon']])
 
 
 
-# set the params for spot list request
-params_route = {'walk': '', 'start': '', 'goal': '', 'order': '', 'car-fuel': ''}
-params_route['car'] = 'only'
-params_route['order'] = 'total_distance'
-# 35km per L
-FUEL_CONSUMPTION = 35
-params_route['car-fuel'] = str(FUEL_CONSUMPTION)
-Distance = np.zeros((NUMBER_OF_STATIONS, NUMBER_OF_STATIONS))
-T_trans = np.zeros((NUMBER_OF_STATIONS, NUMBER_OF_STATIONS))
-for i in range(NUMBER_OF_STATIONS - 1):
-  for j in range(i + 1, NUMBER_OF_STATIONS):
-    # time.sleep(1)
-    if (S_coord[i] != S_coord[j]):
-      params_route['start'] = str(S_coord[i][0]) + ',' + str(S_coord[i][1])
-      params_route['goal'] = str(S_coord[j][0]) + ',' + str(S_coord[j][1])
-      time.sleep(0.65)
-      request = makeRequest(KIND_OF_AIP['route'], params_route)
-      response = getResponse(request)
-      Distance[i][j] = response['items'][0]['summary']['move']['distance']
-      Distance[j][i] = response['items'][0]['summary']['move']['distance']
-      T_trans[i][j] = response['items'][0]['summary']['move']['time']
-      T_trans[j][i] = response['items'][0]['summary']['move']['time']
-    else:
-      Distance[i][j] = 0
-      T_trans[i][j] = 0
+  # set the params for spot list request
+  params_route = {'walk': '', 'start': '', 'goal': '', 'order': '', 'car-fuel': ''}
+  params_route['car'] = 'only'
+  params_route['order'] = 'total_distance'
+  # 35km per L
+  FUEL_CONSUMPTION = 35
+  params_route['car-fuel'] = str(FUEL_CONSUMPTION)
+  Distance = np.zeros((NUMBER_OF_STATIONS, NUMBER_OF_STATIONS))
+  T_trans = np.zeros((NUMBER_OF_STATIONS, NUMBER_OF_STATIONS))
+  for i in range(NUMBER_OF_STATIONS - 1):
+    for j in range(i + 1, NUMBER_OF_STATIONS):
+      # time.sleep(1)
+      if (S_coord[i] != S_coord[j]):
+        params_route['start'] = str(S_coord[i][0]) + ',' + str(S_coord[i][1])
+        params_route['goal'] = str(S_coord[j][0]) + ',' + str(S_coord[j][1])
+        time.sleep(0.65)
+        request = makeRequest(KIND_OF_AIP['route'], params_route)
+        response = getResponse(request)
+        Distance[i][j] = response['items'][0]['summary']['move']['distance']
+        Distance[j][i] = response['items'][0]['summary']['move']['distance']
+        T_trans[i][j] = response['items'][0]['summary']['move']['time']
+        T_trans[j][i] = response['items'][0]['summary']['move']['time']
+      else:
+        Distance[i][j] = 0
+        T_trans[i][j] = 0
 
-writeMatrix(S_coord, './s_coord.csv')
-writeMatrix(Distance, './distance.csv')
-writeMatrix(T_trans, './t_trans.csv')
-writeMatrix(S_info, './s_info.csv')
-writeConst('NUMBER_OF_STATIONS', NUMBER_OF_STATIONS)
-writeConst('FUEL_CONSUMPTION', FUEL_CONSUMPTION)
+  writeMatrix(S_coord, './s_coord.csv')
+  writeMatrix(Distance, './distance.csv')
+  writeMatrix(T_trans, './t_trans.csv')
+  writeMatrix(S_info, './s_info.csv')
+  writeConst('NUMBER_OF_STATIONS', NUMBER_OF_STATIONS)
+  writeConst('FUEL_CONSUMPTION', FUEL_CONSUMPTION)
