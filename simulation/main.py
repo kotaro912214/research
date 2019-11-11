62e82fc7a8c0f52a1b2d1e11cb4c6f5992a92c73
kotaro912214 <kotaro.seki1216@gmail.com>
2019-11-11 18:03:50 +0900

    main.pyの内容を整理

--- simulation/main.py
+++ simulation/main.py
@@ -1,111 +1,66 @@
 from simulation import Simulation
 
-continuous_time = Simulation(params={
-    'NUMBER_OF_EMPLOYEES': 1,
-    'TIME': 60 * 8,
-    'NUMBER_OF_STATIONS': 5,
-    'SELECT_RATIO': 5,
-    'CONFIG_NAME': 'continuous_time',
-    'MAKE_RANDOM_DEMANDS': False,
-    'RELOCATE': True,
-    'CONTINUOUS_TIME': True
-})
 
-# continuous_time.get_all_datas()
-# continuous_time.excute()
-# continuous_time.draw_rsf_graph()
-# continuous_time.draw_rse_graph()
+def check_time_config_affection():
+    for b in [True, False]:
+        continuous_time = Simulation(params={
+            'NUMBER_OF_EMPLOYEES': 1,
+            'TIME': 60 * 1,
+            'NUMBER_OF_STATIONS': 3,
+            'SELECT_RATIO': 5,
+            'CONFIG_NAME': 'continuous_time',
+            'MAKE_RANDOM_DEMANDS': False,
+            'RELOCATE': b,
+            'CONTINUOUS_TIME': True,
+            'ELASTIC_VHECLES': -1,
+            'MU': -2.15,
+            'SIGMA': 1.27
+        })
 
-# no_continuous_time = Simulation(params={
-#     'NUMBER_OF_EMPLOYEES': 1,
-#     'TIME': 60 * 8,
-#     'C_IN': 100,
-#     'C_OUT': 205,
-#     'C_E_FULL': 10000,
-#     'PRICE_PER_15': 205,
-#     'FUEL_CONSUMPTION': 35,
-#     'NUMBER_OF_STATIONS': 5,
-#     'SELECT_RATIO': 5,
-#     'CONFIG_NAME': 'no_continuous_time',
-#     'MAKE_RANDOM_DEMANDS': False,
-#     'RELOCATE': True,
-#     'CONTINUOUS_TIME': False
-# })
+        continuous_time.get_all_datas()
+        continuous_time.excute()
 
-# no_continuous_time.get_all_datas()
-# no_continuous_time.excute()
-# no_continuous_time.draw_rsf_graph()
-# no_continuous_time.draw_rse_graph()
+    continuous_time.draw_rsf_graph()
+    continuous_time.draw_rse_graph()
+
+    for b in [True, False]:
+        no_continuous_time = Simulation(params={
+            'NUMBER_OF_EMPLOYEES': 1,
+            'TIME': 60 * 1,
+            'NUMBER_OF_STATIONS': 3,
+            'SELECT_RATIO': 5,
+            'CONFIG_NAME': 'no_continuous_time',
+            'MAKE_RANDOM_DEMANDS': False,
+            'RELOCATE': b,
+            'CONTINUOUS_TIME': False,
+            'ELASTIC_VHECLES': -1,
+            'MU': -2.15,
+            'SIGMA': 1.27
+        })
+
+        no_continuous_time.get_all_datas()
+        no_continuous_time.excute()
+
+    no_continuous_time.draw_rsf_graph()
+    no_continuous_time.draw_rse_graph()
 
 
-# for mu in [i / 10 for i in range(-30, 35, 1)]:
-#     for sigma in [j / 10 for j in range(0, 21, 1)]:
-#         vms = Simulation(params={
-#             'NUMBER_OF_EMPLOYEES': 1,
-#             'TIME': 60 * 2,
-#             'C_IN': 100,
-#             'C_OUT': 205,
-#             'C_E_FULL': 10000,
-#             'PRICE_PER_15': 205,
-#             'FUEL_CONSUMPTION': 35,
-#             'NUMBER_OF_STATIONS': 5,
-#             'SELECT_RATIO': 5,
-#             'CONFIG_NAME': 'vhecle_mu_sigma2',
-#             'MAKE_RANDOM_DEMANDS': True,
-#             'RELOCATE': True,
-#             'CONTINUOUS_TIME': True,
-#             'VHECLES': 3,
-#             'MU': mu,
-#             'SIGMA': sigma
-#         })
-#         vms.get_all_datas()
-#         vms.excute()
+def check_capa_and_vhecle_ratio():
+    for i in [100, 50, 20, 2]:
+        for j in range(i + 1):
+            elastic_capa = Simulation(params={
+                'NUMBER_OF_EMPLOYEES': 1,
+                'TIME': 60 * 8,
+                'NUMBER_OF_STATIONS': 5,
+                'SELECT_RATIO': 5,
+                'CONFIG_NAME': 'capa' + str(i),
+                'MAKE_RANDOM_DEMANDS': False,
+                'RELOCATE': True,
+                'CONTINUOUS_TIME': True,
+                'ELASTIC_CAPA': j,
+                'MU': -2.15,
+                'SIGMA': 1.27
+            })
 
-# for i in range(11, 21):
-#     vms = Simulation(params={
-#         'NUMBER_OF_EMPLOYEES': 1,
-#         'TIME': 60 * 3,
-#         'C_IN': 100,
-#         'C_OUT': 205,
-#         'C_E_FULL': 10000,
-#         'PRICE_PER_15': 205,
-#         'FUEL_CONSUMPTION': 35,
-#         'NUMBER_OF_STATIONS': 5,
-#         'SELECT_RATIO': i,
-#         'CONFIG_NAME': 'ratio' + str(i),
-#         'MAKE_RANDOM_DEMANDS': True,
-#         'RELOCATE': True,
-#         'CONTINUOUS_TIME': True,
-#         'VHECLES': False,
-#         'MU': -2.15,
-#         'SIGMA': 1.27
-#     })
-#     vms.get_all_datas()
-#     vms.excute()
-
-# static_capa.draw_rsf_graph()
-# static_capa.draw_rse_graph()
-
-dynamic_capa = Simulation(params={
-    'NUMBER_OF_EMPLOYEES': 1,
-    'TIME': 60 * 3,
-    'C_IN': 100,
-    'C_OUT': 205,
-    'C_E_FULL': 10000,
-    'PRICE_PER_15': 205,
-    'FUEL_CONSUMPTION': 35,
-    'NUMBER_OF_STATIONS': 5,
-    'SELECT_RATIO': 5,
-    'CONFIG_NAME': 'capa_final',
-    'MAKE_RANDOM_DEMANDS': True,
-    'RELOCATE': True,
-    'CONTINUOUS_TIME': True,
-    'VHECLES': 2,
-    'MU': -2.15,
-    'SIGMA': 1.27
-})
-
-dynamic_capa.get_all_datas()
-dynamic_capa.excute()
-# dynamic_capa.draw_rsf_graph()
-# dynamic_capa.draw_rse_graph()
+            elastic_capa.get_all_datas()
+            elastic_capa.excute()
