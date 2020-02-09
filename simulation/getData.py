cc800d78b9535808e6d7a3b0e6786517f702e0ca
kotaro912214 <kotaro.seki1216@gmail.com>
2020-02-09 18:30:46 +0900

    add docstring is_exist, read_matrix

--- simulation/getData.py
+++ simulation/getData.py
@@ -25,8 +25,8 @@ def my_round(val, digit=0):
     任意の有効桁数で四捨五入するメソッドを定義．
 
     Args:
-        val: float. 四捨五入する値
-        digit: int. 有効桁数，デフォルトでは0．
+        val: float, 四捨五入する値
+        digit: int, 有効桁数，デフォルトでは0．
 
     Returns:
         四捨五入した値を返す．
@@ -47,10 +47,10 @@ def read_sid(base_path):
     したがってGitHub上では管理しないSID.txtというファイルにSIDを記述し，コードを実行する度にこれを動的に読み込む．
 
     Args:
-        base_path: str. シミュレーションを実行するベースディレクトリのパスを示す．base_pathディレクトリ配下のSID.txtを参照する．
+        base_path: pathlib.PureWindowsPath, シミュレーションを実行するベースディレクトリのパスを示す．base_pathディレクトリ配下のSID.txtを参照する．
 
     Returns:
-        str. 読み取ったSIDを返す．
+        str, 読み取ったSIDを返す．
     """
     sid_path = base_path / 'sid.txt'
     f = open(sid_path)
@@ -62,12 +62,15 @@ def make_request(base_path, api, params):
     """APIへのリクエストを作成するメソッド
 
     Args:
-        base_path: str. シミュレーションを実行するベースディレクトリのパスを示す．
-        aip: str. 利用するapiの種類を識別する文字列．
-            example
+        base_path: pathlib.PureWindowsPath, シミュレーションを実行するベースディレクトリのパスを示す．
+        aip: str, 利用するapiの種類を識別する文字列．
+            example:
+
             '/spot/list?'
-        params: dict. URLで利用するパラメータとその値を格納した辞書．
-            example 
+
+        params: dict, URLで利用するパラメータとその値を格納した辞書．
+            example:
+
             {
                 'category': '0817001002',
                 'coord': '35.689296,139.702089',
@@ -75,10 +78,13 @@ def make_request(base_path, api, params):
                 'limit': '10',
                 'datum': 'tokyo'
             }
+
     Returns:
-        str. 作成したAPIリクエストを実行するためのURLを返す．
-        example
+        str, 作成したAPIリクエストを実行するためのURLを返す．
+        example:
+
         'https://api-challenge.navitime.biz/v1s/sid/spot/list?category=0817001002&coord={35.689296,139.702089}&radius=100&limit=10&datum=tokyo'
+
     """
     base_url = 'https://api-challenge.navitime.biz/v1s/'
     request = base_url + read_sid(base_path) + api
@@ -88,6 +94,14 @@ def make_request(base_path, api, params):
 
 
 def read_matrix(path):
+    """csvファイルを配列として読み込むメソッド
+
+    Args:
+        path: pathlib.PureWindowsPath, 読み込むファイルのパス．
+
+    Returns:
+        list, 読み込むファイルが一行の場合一次元配列を，複数行の場合二次元配列を返す．
+    """
     matrix = []
     with open(path, 'r') as f:
         reader = csv.reader(f)
@@ -100,4 +114,16 @@ def read_matrix(path):
 
 
 def is_exist(file_path):
+    """ファイルが存在するかどうかを返すメソッド
+
+    Args:
+        file_path: pathlib.PureWindowsPath, 確認するファイルのパス．
+
+    Returns:
+        bool, 存在する場合はTrue, しない場合はFalseを返す．
+    """
     return Path(file_path).exists()
+
+
+if (__name__ == "__main__"):
+    print('use this file as a module')
