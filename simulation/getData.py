7f758aa378c121d245322fc2e3a802b60b937fb6
kotaro912214 <kotaro.seki1216@gmail.com>
2020-02-09 17:56:50 +0900

    add read_SID docstring

--- simulation/getData.py
+++ simulation/getData.py
@@ -19,7 +19,7 @@ import pandas as pd
 
 
 def my_round(val, digit=0):
-    """値を四捨五入するメソッド
+    """値を四捨五入するメソッド．
 
     組み込み関数のroundメソッドだと誤差が生じる．
     任意の有効桁数で四捨五入するメソッドを定義．
@@ -30,7 +30,7 @@ def my_round(val, digit=0):
 
     Returns:
         四捨五入した値を返す．
-        digitが0の場合int，0でない場合はfloatを返す．
+        digitが0の場合int，0でない場合はfloatで返す．
     """
     p = 10 ** digit
     if (digit == 0):
@@ -40,6 +40,18 @@ def my_round(val, digit=0):
 
 
 def read_sid(base_path):
+    """SIDを読み込むためのメソッド．
+
+    当たり前のことだが，コードはGitHub上で管理することになる．
+    誤って流出してしまう場合を考慮してコード内に秘匿情報である，SIDを直接書くことはできない．
+    したがってGitHub上では管理しないSID.txtというファイルにSIDを記述し，コードを実行する度にこれを動的に読み込む．
+
+    Args:
+        base_path: str. シミュレーションを実行するベースディレクトリのパスを示す．base_pathディレクトリ配下のSID.txtを参照する．
+
+    Returns:
+        str. 読み取ったSIDを返す．
+    """
     sid_path = base_path / 'sid.txt'
     f = open(sid_path)
     SID = f.read()
